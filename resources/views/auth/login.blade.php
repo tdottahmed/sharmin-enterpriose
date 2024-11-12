a59008b71f7cef5a5bdb6466721ce03b2921e61e
Tanbir Ahmed <pondit.tanbir@gmail.com>
2024-11-13 01:25:11 +0600

    Added: Components buttons, inputs and card

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -114,35 +114,20 @@
                                         <div class="mt-4">
                                             <form action="{{ route('login') }}" method="post">
                                                 @csrf
-                                                <div class="mb-3">
+                                                {{-- <div class="mb-3">
                                                     <label for="username" class="form-label">Username</label>
                                                     <input type="text" class="form-control" name="email"
                                                         id="username" placeholder="Enter username">
-                                                </div>
+                                                </div> --}}
 
-                                                <div class="mb-3">
-                                                    <div class="float-end">
-                                                        <a href="auth-pass-reset-cover.html" class="text-muted">Forgot
-                                                            password?</a>
-                                                    </div>
-                                                    <label class="form-label" for="password-input">Password</label>
-                                                    <div class="position-relative auth-pass-inputgroup mb-3">
-                                                        <input type="password" name="password"
-                                                            class="form-control pe-5 password-input"
-                                                            placeholder="Enter password" id="password-input">
-                                                        <button
-                                                            class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon material-shadow-none"
-                                                            type="button" id="password-addon"><i
-                                                                class="ri-eye-fill align-middle"></i></button>
-                                                    </div>
-                                                </div>
+                                                <x-data-entry.input type="email" name="email" label="Email"
+                                                    type="email" placeholder="Email" required />
 
-                                                <div class="form-check">
-                                                    <input class="form-check-input" type="checkbox" value=""
-                                                        id="auth-remember-check">
-                                                    <label class="form-check-label" for="auth-remember-check">Remember
-                                                        me</label>
-                                                </div>
+                                                <x-data-entry.input type="password" name="password" label="Password"
+                                                    type="password" placeholder="Password" required />
+
+                                                <x-data-entry.input type="checkbox" name="remember"
+                                                    label="Remember Me" />
 
                                                 <div class="mt-4">
                                                     <button class="btn btn-success w-100" type="submit">Sign
